4a53c6b67b0eebd1caa1afbda60aaf53c8bc81d4
nkbadhon <badhon7574@gmail.com>
2022-11-10 17:38:04 +0600

    5th commit

--- index.js
+++ index.js
@@ -87,7 +87,12 @@ async function run() {
             res.send(result);
         });
 
-
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await reviewService.deleteOne(query);
+            res.send(result);
+        })
     }
 
 
