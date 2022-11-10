e44985a0e9b1523b90293d7f36b76794b35d2bf5
nkbadhon <badhon7574@gmail.com>
2022-11-10 20:22:41 +0600

    fina commit

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ async function run() {
         app.post('/services', async (req, res) => {
             const services = req.body;
             const result = await serviceCollection.insertOne(services);
-
+            res.send(result);
         });
 
 
